cc88d15ef77a475a26957b1f3f033dccce7f53c2
Aryan Sinha <aryansinha18@Aryans-MacBook-Air.local>
2026-04-29 23:28:14 +0530

    boolean

---
+++ 4.data_types.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // string
+    $str1 = "this is a string";
+    $str2 = 'this is also a string';
+    // integer
+    $int1 = 1;
+    $int2 = 2;
+    // float
+    $float1 = 1.1;
+    $float2 = 2.2;
+    // boolean
+    $bool1 = true;
+    $bool2 = false;
+
+    echo "$str1 <br>";
+    echo "$str2 <br>";
+    echo "$int1 <br>";
+    echo "$int2 <br>";
+    echo "$float1 <br>";
+    echo "$float2 <br>";
+    echo "$bool1 <br>";
+    echo "$bool2 <br>";
+    ?>
+</body>
+</html>
