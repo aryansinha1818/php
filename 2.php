738c85be92c30865496c14b65ff41e2f15d776de
Aryan Sinha <aryansinha18@Aryans-MacBook-Air.local>
2026-04-29 23:24:49 +0530

    variables

---
+++ 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo("hello duniya!");
+        echo"<br>";
+        echo "hello hello";
+        echo"<hr>";
+        echo "<h1> this is h1 tag</h1>";
+        echo "<p> ths is my site using p tag</p>"
+    ?>
+</body>
+</html>
---
+++ 3.variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variables</title>
+</head>
+<body>
+    <?php
+    $name = "John";
+    $name2 = "Raj";
+       echo"the $name is John and $name2 is his friend";
+       echo"<br>";
+       $name2 = "Rishabh";
+       echo"$name2 is a very good person";
+    ?>
+</body>
+</html>
