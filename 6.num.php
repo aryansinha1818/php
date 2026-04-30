fdd625ec0f7dd17640c9c1f344da78c1518bb926
Aryan Sinha <aryansinha18@Aryans-MacBook-Air.local>
2026-04-30 11:22:00 +0530

    math function add round min max

---
+++ 6.num.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<br>";
+    echo "<br>";
+    echo 40;
+    echo "&nbsp this is int";
+
+    echo "<br>";
+    echo "<br>";
+    echo 4.503;
+
+    echo "<br>";
+    echo "<br>";
+    echo "this is add &nbsp";
+    echo 4+3+10;
+    
+    echo "<br>";
+    echo "<br>";
+    echo "mod &nbsp";
+    echo "10%3 is &nbsp", 10%3;
+
+    echo "<br>";
+    echo "<br>";
+    echo "Variables &nbsp";
+    $num = 10;
+    echo "increment ++ --", $num++;
+    echo "<br>";
+    echo $num;
+
+    echo "<br>";
+    echo "<br>";
+    echo "math functions &nbsp";
+    echo "<br>";
+    echo "absolute &nbsp", pow(2,4);
+
+    echo "<br>";
+    echo "<br>";
+    echo "max min &nbsp";
+    echo "<br>";
+    echo max(2,4);
+    echo "<br> min &nbsp";
+    echo min(2,4);
+    ?>
+</body>
+</html>
